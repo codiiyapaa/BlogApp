fd3ff9e8d72e4af1acb576e3dfcb1375b2fcc3ef
Paras Bansal <67871592+codiiyapaa@users.noreply.github.com>
2021-01-10 06:54:58 +0530

    db is connected

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+require('dotenv').config();
+
 const express=require('express');
 const bodyparser=require('body-parser');
 const mongoose=require('mongoose');
 const app=express();
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
+mongoose.Promise=global.Promise;
+mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true }).then(()=>
+    {console.log("DB is connected");}    
+).catch((err)=>{
+    console.log(err);
+    process.exit();
+})
+
 app.get('/',(req,res)=>{
     res.send("I have just started Node.js");
 });
-
+// app.get('/check',(req,res)=>{
+//     res.send("checking");
+// })
 const Port=3000;
 
 app.listen(Port,(err)=>{
